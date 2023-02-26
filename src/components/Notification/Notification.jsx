2a34fa65df740f16d92117c748cf1b6bc0af0367
SerhiiMukhin <110244388+SerhiiMukhin@users.noreply.github.com>
2023-02-26 20:52:51 +0200

    fix

--- src/components/Notification/Notification.jsx
+++ src/components/Notification/Notification.jsx
@@ -1,7 +1,10 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import css from './Notification.module.css';
 
-export const Notification = ({ message }) => <h3>{message}</h3>;
+export const Notification = ({ message }) => (
+  <h3 className={css.message}>{message}</h3>
+);
 
 Notification.propTypes = {
   message: PropTypes.string.isRequired,
